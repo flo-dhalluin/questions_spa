4805c51eb52d27d47feece3f97053051eb9b27ee
Florent D'halluin <flal@melix.net>
2016-01-29 22:15:19 +0100

    end points to post an answer to a question

--- questions/urls.py
+++ questions/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url, patterns
 #from rest_framework.urlpatterns import format_suffix_patterns
-from .views import QuestionListView, QuestionDetailView
+from .views import QuestionListView, QuestionDetailView, AnswerCreateView
 
 
 urlpatterns = patterns('',
                        url(r'^$', QuestionListView.as_view(), name='question-list'),
                        url(r'^question/(?P<slug>[-_\w]+)$', QuestionDetailView.as_view(), name='question-detail'),
+                       url(r'^question/(?P<slug>[-_\w]+)/answer/$', AnswerCreateView.as_view(), name='create-answer'),
                        )
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from .serializers import QuestionSerializer
+from .serializers import AnswerSerializer
 from .serializers import QuestionWithAnswersSerializer
 from .models import Question
 
@@ -15,3 +16,12 @@ class QuestionDetailView(generics.RetrieveAPIView):
     # by default, the "RetrieveView" uses pk field to get one element
     # let's use slug instead
     lookup_field = 'slug'
+
+class AnswerCreateView(generics.CreateAPIView):
+    serializer_class = AnswerSerializer
+
+    def perform_create(self, serializer):
+        print(self.kwargs)
+        q = Question.objects.get(slug = self.kwargs['slug'])
+        serializer.save(question=q)
+        
